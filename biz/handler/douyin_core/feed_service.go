31a0e2f2d76a0dbadbec8d20c2aeaa0345458eb0
zhangjiansheng <zjsd@mail.ustc.edu.cn>
2023-02-23 20:14:39 +0800

    small adjustment

--- biz/handler/douyin_core/feed_service.go
+++ biz/handler/douyin_core/feed_service.go
@@ -78,6 +78,11 @@ func FeedService(req douyin_core.DouyinFeedRequest) douyin_core.DouyinFeedRespon
 	for i := 0; i < len(videos); i++ {
 		users := make([]*douyin_core.User, 0)
 		tx.Where("id = ?", videos[i].UserId).Find(&users)
+		userfavvideo := make([]*douyin_core.UserFavVideos, 0)
+		tx.Where("userid = ? and videoid = ", videos[i].UserId, videos[i].Id).Find(&userfavvideo)
+		if len(userfavvideo) != 0 {
+			videos[i].IsFavorite = true
+		}
 		videos[i].Author = users[0]
 	}
 	tx.Commit()
